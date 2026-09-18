aa1ee1c3a9fa6ea323c8b99c3bfc019c15126213
Louis7034 <louis37.bobet@gmail.com>
2026-09-19 00:31:05 +0200

    create navbar

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Accueil } from './features/accueil/accueil';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Accueil],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('front');
